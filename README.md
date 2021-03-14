c7dd9cf11c8b853b1f715cc31bfe75fad53ab5b4
S <ranjithkumar.s@dxc.com>
2021-03-14 17:32:16 +0530

    Add connect and disconnect event handlers

---
+++ README.md
@@ -0,0 +1,13 @@
+WEB SOCKET:
+-----------
+
+Server will be able to accept the connection and client will be make connection then
+we will have persistent connection and we can send data back and forth whether its data
+from server to the client or data from the client to the server . that is the beauty of 
+web socketio.
+
+
+http://localhost:3000/socket.io/socket.io.js -  if you run our app then load this js file in browser, you can see the long javascript library.here, we can get all the connection code from server to client or vice versa. 
+Communication through events
+Events can be emitted from either the client or the server and server or client can listen for events.
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
-// Remember this one does not need to be installed. it has a build in module 
+// Remember this two does not need to be installed. it has a build in module 
 const path = require("path");
+const http = require('http');
 
 // __dirname - to reference to the current directory 
 // (C:\Users\rs281\OneDrive - DXC Production\Desktop\ranjith\nodejs\nodejs-chat\server)
@@ -15,15 +16,35 @@ const publicPath  = path.join(__dirname,  '../public'); //C:\Users\rs281\OneDriv
 // console.log(publicPath);
 
 const express = require('express');
-const { static } = require("express");
+const socketIO = require('socket.io');
+const { Socket } = require("dgram");
 
 const app = express();
+
+const port = 3000 || process.env.PORT;
+/**
+ * it is similar to app.listenlisten(port, () => {
+    console.log(`Server is up on port ${port}`)
+});
+ * Http is actually used behind the scenes for express
+ */
+const server = http.createServer(app);
+
+// Here, we have access to the server via server variable
+const io = socketIO(server);
+
 
 // this is configuration of express static middleware
 app.use(express.static(publicPath));
 
-const port = 3000 || process.env.PORT;
-app.listen(port, () => {
+// io.on() - register an event listener
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 });
 
@@ -31,3 +52,4 @@ app.listen(port, () => {
 
 
 
+ 
